a85ea55780252d0bb84e137bea784899432d04ec
jaromir <jaromirblu@gmail.com>
2022-08-11 15:28:01 +0200

    limit table elements

--- src/components/workpanel/OutputTable.tsx
+++ src/components/workpanel/OutputTable.tsx
@@ -1,14 +1,15 @@
 import "./OutputTable.css";
 
-
 interface Props {
     data: any[][]
 }
 
 // display query results (data from props) as table
 const OutputTable = (props: Props) => {
+    const limit: number = 1000;
+
     const column_names = props.data.slice(0, 1);
-    const data = props.data.slice(1, props.data.length);
+    const data = props.data.slice(1, limit >= props.data.length ? props.data.length : limit); // limit number of max rows
 
     return (
         <div className="OutputTableDiv">
